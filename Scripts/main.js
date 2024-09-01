ba64fa97d8a06d3b127760faf3eb40be52a32868
zhassan2312 <zhassan.ce44ceme@student.nust.edu.pk>
2024-09-01 15:16:49 +0500

    combining css files

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,3 +1,194 @@
+let projects = [
+    {
+        video: './Assets/Portfolio/Kolt Landing page.mp4',
+        title: 'Kolt Landing Page',
+        type: 'Branding'
+    },
+    {
+        video: './Assets/Portfolio/reddit (2).mp4',
+        title: 'Reddit Redesign',
+        type: 'UX Design'
+    },
+    {
+        video: './Assets/Portfolio/Turismo Dashboards.mp4',
+        title: 'Turismo Dashboards',
+        type: 'UI Design'
+    },
+    {
+        video: './Assets/Portfolio/twitch redesign.mp4',
+        title: 'Twitch Redesign',
+        type: 'UX Design'
+    },
+    {
+        video: './Assets/Portfolio/UXRKhan Portfolio.mp4',
+        title: 'UXR Khan Portfolio',
+        type: 'Web Design & Development'
+    },
+    {
+        video: './Assets/Portfolio/Almaida food app.mp4',
+        title: 'Almaida Food App',
+        type: 'App Design'
+    },
+    {
+        video: './Assets/Portfolio/ChemTech.mp4',
+        title: 'ChemTech',
+        type: 'App Design'
+    },
+    {
+        video: './Assets/Portfolio/slack redesign.mp4',
+        title: 'Slack Redesign',
+        type: 'UX Design'
+    },
+    {
+        video: './Assets/Portfolio/Plants animation websites.mp4',
+        title: 'Plantate',
+        type: 'Interaction Desing'
+    },
+    {
+        video: './Assets/Portfolio/Codexcue.mp4',
+        title: 'Codexcue Landing Page',
+        type: 'Interaction Design and development'
+    },
+    {
+        video: './Assets/Portfolio/Ebay redesign.mp4',
+        title: 'Ebay Redesign',
+        type: 'UX Design'
+    },
+];
+
+let projectHTML = `
+    <div id="horizontalHeader">
+        <button>reset</button>
+        <h1 data-text="Code X Design">Code X Design</h1>
+        <p data-text="By Zohaib Hassan">By Zohaib Hassan</p>
+    </div>
+`;
+let portfolioSlider = document.querySelector('.portfolioSlider');
+
+projects.forEach((project) => {
+    projectHTML += `
+        <div id="portfolio">
+            <h1 id="portfolioHeading">${project.title}</h1>
+            <video rel="preload" id="portfolioVideo" src="${project.video}" type="video/mp4"></video>
+            <p id="portfolioType">${project.type}</p>
+        </div>
+    `;
+});
+
+portfolioSlider.innerHTML = projectHTML;
+
+
+const portfolioVideos = document.querySelectorAll("#portfolioVideo");
+
+portfolioVideos.forEach(video => {
+    video.addEventListener("mouseenter", () => {
+        video.play();
+    });
+
+    video.addEventListener("mouseleave", () => {
+        video.pause();
+        video.currentTime = 0; // Reset video to start
+    });
+});
+
+
+    const loader = document.getElementById('loader');
+    const loaderText = document.getElementById('loaderText');
+
+    // Function to type out text
+    function typeText(element, text, duration, onComplete) {
+        let index = 0;
+        const interval = duration / text.length;
+        const typing = setInterval(() => {
+            element.textContent += text[index];
+            index++;
+            if (index === text.length) {
+                clearInterval(typing);
+                if (onComplete) onComplete();
+            }
+        }, interval);
+    }
+
+    // Initial text animation
+    loaderText.textContent = "";
+    typeText(loaderText, "hello,", 600, () => {
+        gsap.to(loaderText, {
+            duration: 0.5,
+            opacity: 0,
+            delay: 0.5,
+            onComplete: () => {
+                loaderText.textContent = "";
+                gsap.to(loaderText, {
+                    duration: 0,
+                    opacity: 1,
+                    onComplete: () => {
+                        loaderText.textContent = "welcome";
+                        gsap.to(loaderText, {
+                            duration: 0.2,
+                            opacity: 0,
+                            delay: 0.1,
+                            onComplete: () => {
+                                loaderText.textContent = "";
+                                gsap.to(loaderText, {
+                                    duration: 0,
+                                    opacity: 1,
+                                    onComplete: () => {
+                                        loaderText.textContent = "to";
+                                        gsap.to(loaderText, {
+                                            duration: 0.2,
+                                            opacity: 0,
+                                            delay: 0.1,
+                                            onComplete: () => {
+                                                loaderText.textContent = "";
+                                                gsap.to(loaderText, {
+                                                    duration: 0,
+                                                    opacity: 1,
+                                                    onComplete: () => {
+                                                        
+                                                                        loaderText.textContent = "Code x Design";
+                                                                        gsap.to(loaderText, {
+                                                                            duration: 0.2,
+                                                                            opacity: 0,
+                                                                            delay: 0.1,
+                                                                            onComplete: () => {
+                                                                                loaderText.textContent = "";
+                                                                                gsap.to(loaderText, {
+                                                                                    duration: 0,
+                                                                                    opacity: 1,
+                                                                                    onComplete: () => {
+                                                                                        loaderText.textContent = "By Zohaib";
+                                                                                        gsap.to(loaderText, {
+                                                                                            duration: 0.2,
+                                                                                            opacity: 0,
+                                                                                            delay: 0.1,
+                                                                                            onComplete: () => {
+                                                                                                // Collapse loader to the left and reveal main content from the right
+                                                                                                gsap.to(loader, {
+                                                                                                    duration: 0.5,
+                                                                                                    x: "-100%",
+                                                                                                    ease: "power2.inOut",
+                                                                                                });
+                                                                                            }
+                                                                                        });
+                                                                                    }
+                                                                                });
+                                                                            }
+                                                                        });
+                                                                    }
+                                                                });
+                                                            }
+                                                        });
+                                                    }
+                                                });
+                                            }
+                                        });
+                                    }
+                                });
+                            }
+                        });
+                    
+    });
+
 gsap.registerPlugin(ScrollTrigger);
 const locoScroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
@@ -411,3 +602,245 @@ ScrollTrigger.create({
         currentScroll = scrollTop;
     }
 });
+
+const contactButton = document.querySelector('.contactButton2');
+const talkButton = document.querySelector('.buttonWrapper1');
+const dialogBox = document.getElementById('dialogBox');
+const closeButton = document.getElementById('closeButton');
+const overlay = document.getElementById('overlay');
+
+const steps = document.querySelectorAll('.step');
+let currentStep = 0;
+
+function showStep(stepIndex) {
+    gsap.to(steps, { display: 'none', opacity: 0, duration: 0.5 });
+    gsap.to(steps[stepIndex], { display: 'block', opacity: 1, duration: 0.5, onComplete: () => {
+        // Animate text elements within the step
+        const textElements = steps[stepIndex].querySelectorAll('h2, p, .input-container, .option-buttons, .option-button');
+        textElements.forEach((el, index) => {
+            gsap.fromTo(el, { y: -50, opacity: 0 }, { y: 0, opacity: 1, duration: 0.5, delay: index * 0.1 });
+        });
+    }});
+}
+
+contactButton.addEventListener('click', function() {
+    gsap.to([overlay, dialogBox], {
+        display: 'block',
+        opacity: 1,
+        duration: 0.5,
+        ease: 'power1.out',
+        stagger: 0.1 // Stagger the animations slightly
+    });
+    showStep(0); // Show the first step
+});
+talkButton.addEventListener('click', function() {
+    gsap.to([overlay, dialogBox], {
+        display: 'block',
+        opacity: 1,
+        duration: 0.5,
+        ease: 'power1.out',
+        stagger: 0.1 // Stagger the animations slightly
+    });
+    showStep(0); // Show the first step
+});
+
+closeButton.addEventListener('click', function() {
+    gsap.to([overlay, dialogBox], {
+        opacity: 0,
+        duration: 0.5,
+        ease: 'power1.out',
+        onComplete: function() {
+            overlay.style.display = 'none';
+            dialogBox.style.display = 'none';
+        }
+    });
+});
+
+document.getElementById('nextToStep2').addEventListener('click', function() {
+    showStep(1);
+});
+
+document.getElementById('nextToStep3').addEventListener('click', function() {
+    showStep(2);
+});
+
+document.getElementById('nextToStep4').addEventListener('click', function() {
+    showStep(3);
+});
+
+document.getElementById('nextToStep5').addEventListener('click', function() {
+    showStep(4);
+});
+
+document.getElementById('submitForm').addEventListener('click', function() {
+    showStep(5);
+});
+
+
+var cursor = document.querySelector('.cursor-dot'),
+    follower = document.querySelector('.cursor-bg');
+
+var posX = 0,
+    posY = 0,
+    mouseX = 0,
+    mouseY = 0;
+
+TweenMax.to({}, 0.016, {
+    repeat: -1,
+    onRepeat: function() {
+        posX += (mouseX - posX) / 9;
+        posY += (mouseY - posY) / 9;
+
+        TweenMax.set(follower, {
+            css: {
+                left: posX - 36,
+                top: posY - 36
+            }
+        });
+
+        TweenMax.set(cursor, {
+            css: {
+                left: mouseX,
+                top: mouseY
+            }
+        });
+    }
+});
+
+const items = document.querySelectorAll('#portfolioVideo,#testimonialCard');
+
+window.addEventListener("mousemove", function(e) {
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+});
+
+items.forEach(item => {
+    item.addEventListener('mouseenter', () => {
+        cursor.classList.add('active');
+        follower.classList.add('active');
+    });
+
+    item.addEventListener('mouseleave', () => {
+        cursor.classList.remove('active');
+        follower.classList.remove('active');
+    });
+});
+
+
+const buttons = document.querySelectorAll('.primaryButton','#horizontalHeader button','#menu img');
+
+buttons.forEach(button => {
+    const buttonText = button.querySelector('span'); // Assuming the text is inside a <span> element
+
+    let boundingRect = button.getBoundingClientRect();
+
+    window.addEventListener('resize', () => {
+        boundingRect = button.getBoundingClientRect();
+    });
+
+    button.addEventListener('mousemove', (e) => {
+        const mousePosX = e.clientX - boundingRect.left;
+        const mousePosY = e.clientY - boundingRect.top;
+
+        const offsetX = (mousePosX - boundingRect.width / 2) * 0.4;
+        const offsetY = (mousePosY - boundingRect.height / 2) * 0.4;
+
+        gsap.to(button, {
+            x: offsetX,
+            y: offsetY,
+            stagger: -0.1,
+            duration: 0.8,
+            ease: 'power3.out',
+        });
+
+        gsap.to(buttonText, {
+            x: offsetX * 0.5, // Adjusted to move less than the button itself
+            y: offsetY * 0.5, // Adjusted to move less than the button itself
+            stagger: -0.1,
+            duration: 0.8,
+            ease: 'power3.out',
+        });
+    });
+
+    button.addEventListener('mouseleave', () => {
+        gsap.to(button, {
+            x: 0,
+            y: 0,
+            stagger: -0.1,
+            duration: 0.8,
+            ease: 'elastic.out(1, 0.3)',
+        });
+
+        gsap.to(buttonText, {
+            x: 0,
+            y: 0,
+            stagger: -0.1,
+            duration: 0.8,
+            ease: 'elastic.out(1, 0.3)',
+        });
+    });
+
+});
+
+const contactButtons = document.querySelectorAll('.contactButton1, .contactButton2','.contact-link-item','#menu img','#horizontalHeader button','.next-button','.option-button');
+
+contactButtons.forEach((button) => {
+    const text = button.querySelector('span');
+    const textColor=text.style.color;
+
+    button.addEventListener('mousemove', (e) => {
+        const rect = button.getBoundingClientRect();
+        const x = e.clientX - rect.left - rect.width / 2;
+        const y = e.clientY - rect.top - rect.height / 2;
+
+        // Button magnetic effect
+        gsap.to(button, {
+            duration: 0.3,
+            x: x * 0.1,
+            y: y * 0.1,
+            stagger: -0.1,
+            ease: "power2.out"
+        });
+
+        // Text magnetic effect
+        gsap.to(text, {
+            duration: 0.3,
+            x: x *0.4,
+            y: y *0.4,
+            color: "white",
+            stagger: -0.1,
+            ease: "power2.out"
+        });
+    });
+
+    button.addEventListener('mouseleave', () => {
+        // Reset button position
+        gsap.to(button, {
+            duration: 0.5,
+            x: 0,
+            y: 0,
+            ease: "elastic.out(1, 0.5)"
+        });
+
+        // Reset text position
+        gsap.to(text, {
+            duration: 0.5,
+            x: 0,
+            y: 0,
+            color: textColor,
+            ease: "elastic.out(1, 0.5)"
+        });
+    });
+});
+
+
+const optionButtons = document.querySelectorAll('.option-button');
+
+    optionButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            // Remove 'selected' class from all buttons
+            optionButtons.forEach(btn => btn.classList.remove('selected'));
+            // Add 'selected' class to the clicked button
+            this.classList.add('selected');
+        });
+    });
